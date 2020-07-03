0a0eeaf224260b1e67fe8ddae6b2ed07972280ca
Nicolas <alternative.rvb@gmail.com>
2020-07-04 01:56:14 +0200

    MAJ Majeur

---
+++ js/side-nav.js
@@ -0,0 +1,10 @@
+var sideNavAction = document.getElementById("mySidenav");
+
+function openNav() {
+    sideNavAction.classList.toggle("open");
+
+}
+
+function closeNav() {
+    sideNavAction.classList.remove("open");
+}
